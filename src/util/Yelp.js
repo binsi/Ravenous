2ba390d7207c3015523af238b1763dd1c5603a5e
Bina <binasteinert@gmail.com>
2018-04-22 14:25:39 +0200

    Complete commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'OxufnSdu5yEN9e-dItQsNfwzMFcorfsQCqZrJhBZt7_77nzIdKtLWM5wMQMhBBnkUpT-BbXj178tqQ4bAEplCTx76d4-Gbix_evbt7_O211ghQOI4J8cqTgoJ2bcWnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
